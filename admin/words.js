57f2c1bf8997974026a3617092fd7d5cd25d3623
Klaus Ullmer <klaus.ullmer@gmx.de>
2021-02-10 15:44:29 +0100

    Klaus1

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"klaus1 adapter settings": {
+		"en": "Adapter settings for klaus1",
+		"de": "Adaptereinstellungen für klaus1",
+		"ru": "Настройки адаптера для klaus1",
+		"pt": "Configurações do adaptador para klaus1",
+		"nl": "Adapterinstellingen voor klaus1",
+		"fr": "Paramètres d'adaptateur pour klaus1",
+		"it": "Impostazioni dell'adattatore per klaus1",
+		"es": "Ajustes del adaptador para klaus1",
+		"pl": "Ustawienia adaptera dla klaus1",
+		"zh-cn": "klaus1的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
